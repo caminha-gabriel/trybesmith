20bca97eeda940ff37f61ce4ea57657e760531e0
Caminha <gabrielcaminha255@gmail.com>
2022-09-19 18:33:22 -0300

    feat: implemented middlewares and products route to app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import errorMiddleware from './middlewares/error.middleware';
+import ProductsRoute from './routes/products.routes';
 
 const app = express();
 
 app.use(express.json());
 
+app.use(ProductsRoute);
+
+app.use(errorMiddleware);
 export default app;
