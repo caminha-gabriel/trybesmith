db694cce820636185bd249e497083c7383046b59
Caminha <gabrielcaminha255@gmail.com>
2022-09-19 19:34:12 -0300

    feat: implemented users route on app

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import errorMiddleware from './middlewares/error.middleware';
 import ProductsRoute from './routes/products.routes';
+import UsersRoute from './routes/users.routes';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(ProductsRoute);
+app.use(UsersRoute);
 
 app.use(errorMiddleware);
 export default app;
