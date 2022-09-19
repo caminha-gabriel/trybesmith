a32b6a80587de140d75439b0f9cfb7615200ab5b
Caminha <gabrielcaminha255@gmail.com>
2022-09-19 20:06:01 -0300

    feat: implemented orders route on app

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import errorMiddleware from './middlewares/error.middleware';
 import ProductsRoute from './routes/products.routes';
 import UsersRoute from './routes/users.routes';
+import OrdersRoute from './routes/orders.routes';
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 
 app.use(ProductsRoute);
 app.use(UsersRoute);
+app.use(OrdersRoute);
 
 app.use(errorMiddleware);
 export default app;
